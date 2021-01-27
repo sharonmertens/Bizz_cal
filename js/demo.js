f36c1d0012ce402de9ef39b313c00019903f6dc4
Sharon Mertens <sharon.mertens@gmail.com>
2021-01-26 21:08:40 -0500

    created calendar

---
+++ js/demo.js
@@ -0,0 +1,37 @@
+var events = [
+    {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
+    {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
+    {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+    {'Date': new Date(2019, 5, 22), 'Title': 'Ticket Boost Training', 'Link': 'https://events.bizzabo.com/213003'},
+    {'Date': new Date(2019, 11, 31), 'Title': 'Roadshow - Coming to a City Near You', 'Link': 'https://events.bizzabo.com/212296'},
+    {'Date': new Date(2020, 2, 1), 'Title': 'Event Calendar Training - 03/01/20', 'Link': 'https://events.bizzabo.com/213004'},
+    {'Date': new Date(2019, 9, 18), 'Title': 'RSVP Event', 'Link': 'https://events.bizzabo.com/219124'},
+    {'Date': new Date(2020, 5, 8), 'Title': 'LS Retail - CF-100 Introduction', 'Link': 'https://events.bizzabo.com/224123'},
+    {'Date': new Date(2020, 2, 2), 'Title': 'Event Calendar Training - 03/02/20', 'Link': 'https://events.bizzabo.com/213100'},
+    {'Date': new Date(2021, 0, 1), 'Title': 'Test Mux Event', 'Link': 'https://events.bizzabo.com/286891'},
+    {'Date': new Date(2020, 0, 6), 'Title': 'Roadshow - Brooklyn', 'Link': 'https://events.bizzabo.com/212295'},
+    {'Date': new Date(2020, 2, 9), 'Title': 'Roadshow - Corralitos', 'Link': 'https://events.bizzabo.com/212293'},
+    {'Date': new Date(2020, 5, 8), 'Title': 'Roadshow Event Series', 'Link': 'https://events.bizzabo.com/212270'},
+    {'Date': new Date(2020, 4, 4), 'Title': 'Roadshow - Scotts Valley', 'Link': 'https://events.bizzabo.com/212291'},
+    {'Date': new Date(2019, 8, 2), 'Title': 'Bizzabo + 3rd Party Embeds', 'Link': 'https://events.bizzabo.com/213554'},
+    {'Date': new Date(2020, 1, 3), 'Title': 'Opportunity Event', 'Link': 'https://events.bizzabo.com/219546'},
+    {'Date': new Date(2020, 5, 1), 'Title': 'Client Services @ Bizzabo', 'Link': 'https://events.bizzabo.com/213815'},
+    {'Date': new Date(2020, 3, 6), 'Title': 'Roadshow - Santa Cruz', 'Link': 'https://events.bizzabo.com/212292'},
+    {'Date': new Date(2021, 0, 1), 'Title': 'olutions @ Bizzabo', 'Link': 'https://events.bizzabo.com/solutions'},
+    {'Date': new Date(2019, 3, 4), 'Title': 'BizzaHealth HQ', 'Link': 'https://events.bizzabo.com/213239'},
+    {'Date': new Date(2019, 2, 3), 'Title': 'Event Calendar Training - 03/03/20', 'Link': 'https://events.bizzabo.com/213128'},
+    {'Date': new Date(2021, 1, 3), 'Title': 'Salesforce Tower SF', 'Link': 'https://events.bizzabo.com/212294'},
+    {'Date': new Date(2019, 8, 1), 'Title': 'Side by Side Ticket Showcase', 'Link': 'https://events.bizzabo.com/214906'},
+    {'Date': new Date(2019, 6, 9), 'Title': 'Today is the day!', 'Link': 'https://events.bizzabo.com/216719'},
+    
+  
+  
+  
+  
+  
+    
+  ];
+  var settings = {};
+  var element = document.getElementById('caleandar');
+  caleandar(element, events, settings);
+  
